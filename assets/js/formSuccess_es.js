974eb5edae53d11b62d99c6e27c2a51bf634a93a
Pedro M. Dominguez <84252067+xtcedro@users.noreply.github.com>
2025-01-03 12:09:47 -0600

    Delete assets/js/formSuccess_es.js

--- assets/js/formSuccess_es.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const urlParams = new URLSearchParams(window.location.search);
-    if (urlParams.get('success') === '1') {
-        const messageDiv = document.createElement('div');
-        messageDiv.className = 'success-message';
-        messageDiv.textContent = '¡Tu mensaje ha sido enviado con éxito!';
-        document.body.prepend(messageDiv);
-    }
-});
